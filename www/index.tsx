e89d896769133e3f76279a8961645d082dd16cb0
uncenter <uncenter@uncenter.dev>
2026-05-17 19:08:27 -0500

    feat(www): note that advanced attr now works in firefox

--- www/index.tsx
+++ www/index.tsx
@@ -71,8 +71,10 @@ export default () => {
 				<div class="warning">
 					<p>
 						Your browser does not support advanced{" "}
-						<code>attr()</code>. Try viewing this page on Chrome 133
-						(beta) or later.
+						<code>attr()</code>. Try viewing this page in Chrome
+						version 133 or later. In Firefox version 149 or later,
+						enable the <code>layout.css.attr.enabled</code>{" "}
+						preference in <code>about:config</code>.
 					</p>
 					<a href="https://github.com/uncenter/gusty">
 						View project on GitHub.
